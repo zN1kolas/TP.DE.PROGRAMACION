343deda9688eb1337a8b02813819d8009b4d141a
zN1kolas <nicoarumay8@gmail.com>
2023-09-20 20:09:03 -0300

    Todo hecho con funciones

--- Nicolas Arumay, Lautaro Pizarro l Ejercicio 15.c
+++ Nicolas Arumay, Lautaro Pizarro l Ejercicio 15.c
@@ -3,39 +3,58 @@
 #include <string.h>
 #include <time.h>
 
-void MateriasPromedio (int Notas[3][5], float Suma, float Promedios[]);
-void PorcenMaterias (int Notas[3][5], int cont, float Porcentaje[]);
+void Cargar (int Notas[3][5]); //Cargar matriz de notas
+void Mostrar (char Materias[5][20], char Trim[3][10], int Notas[3][5]);
+void MateriasProm(int Notas[3][5],float Promedios[4], char Materias[5][20]);
+void PorcentAprob(int Notas[3][5], float Porcentaje[4], char Trim[3][10]);
+void PromGeneral (float Promedios[4]);
+
 
 int main()
 {
     char Materias[5][20]={"Matematicas", "Lengua", "Ingles", "Geografia", "Biologia"};
-    char Trim[3][10]={"Trim 1", "Trim 2", "Trim 3"};
-    int cont, Notas[3][5], j=0;
-    char Nombre[10];
-    float Suma=0, Promedios[4], Porcentaje[3], Suma1=0;
+    char Trim[3][10]={"Trim 1", "Trim 2", "Trim 3"}, Nombre[20];
+    int Notas[3][5];
+    float Promedios[4], Porcentaje[4];
 
-        printf("Ingrese su nombre: \n");
-        fflush(stdin);
-        gets(Nombre);
+    printf("Ingrese su nombre: \n");
+    fflush(stdin);
+    gets(Nombre);
+    srand(time(NULL));
+    system("cls");
 
-        srand(time(NULL));
-        system("cls");
-        //Cargar matriz de notas
-        for(int i=0; i<3; i++)
+    Cargar (Notas);
+    Mostrar (Materias, Trim, Notas);
+    printf("Nombre completo: %s\n", Nombre);
+    //Punto a)
+    printf("a)\n");
+    MateriasProm (Notas, Promedios, Materias);
+    printf("b)\n");
+    PorcentAprob (Notas, Porcentaje, Trim);
+    printf("c)\n");
+    PromGeneral (Promedios);
+
+}
+
+void Cargar (int Notas[3][5])
+{
+    for(int i=0; i<3; i++)
+    {
+        for(int j=0; j<5; j++)
         {
-            for(int j=0; j<5; j++)
-            {
-                Notas[i][j]= rand()%10+1;
-            }
+            Notas[i][j]= rand()%10+1;
         }
-        printf("\n");
-        //Imprimir tabla
-        for(int i=0; i<5; i++)
-        {
-            printf("\t");
-            printf("%8s", Materias[i]);
+    }
 
-        }
+}
+void Mostrar (char Materias[5][20], char Trim[3][10], int Notas[3][5])
+{
+    for(int i=0; i<5; i++)
+    {
+        printf("\t");
+        printf("%8s", Materias[i]);
+
+    }
         printf("\n");
         for(int i=0; i<3; i++)
         {
@@ -47,36 +66,11 @@ int main()
             }
             printf("\n");
         }
-
-        //Promedios
-        MateriasPromedio(Notas, Suma, Promedios);
-        PorcenMaterias(Notas, cont, Porcentaje);
-
-
-
-        printf("\na)\n");
-        printf("Nombre completo: %s\n", Nombre);
-        printf("Promedio de Matematicas: %.2f\n", Promedios[0]);
-        printf("Promedio de Lengua: %.2f\n", Promedios[1]);
-        printf("Promedio de Ingles: %.2f\n", Promedios[2]);
-        printf("Promedio de Geografia: %.2f\n", Promedios[3]);
-        printf("Promedio de Biologia: %.2f\n", Promedios[4]);
-
-        printf("\nb)\n");
-        printf("Trim 1: %2.f%c\n", Porcentaje[0], 37);
-        printf("Trim 2: %2.f%c\n", Porcentaje[1], 37);
-        printf("Trim 3: %2.f%c\n", Porcentaje[2], 37);
-
-        printf("\nc)\n");
-        PorcenGeneral(Suma1, Promedios);
-        system("pause");
-        system("cls");
-
-
-    }
-
-void MateriasPromedio(int Notas[3][5], float Suma, float Promedios[4])
+    printf("\n");
+}
+void MateriasProm(int Notas[3][5],float Promedios[4], char Materias[5][20])
 {
+    float Suma=0;
     int j=0;
         while(j<5)
         {
@@ -86,34 +80,36 @@ void MateriasPromedio(int Notas[3][5], float Suma, float Promedios[4])
                 Suma=Suma+Notas[i][j];
             }
             Promedios[j]=Suma/3;
+            printf("Promedio de %s: %.2f\n", Materias[j], Promedios[j]);
             j++;
         }
 }
-
-void PorcenMaterias(int Notas[3][5], int cont, float Porcentaje[4])
+void PorcentAprob(int Notas[3][5], float Porcentaje[4], char Trim [3][10])
 {
-    int j=0;
+    int j=0, cont;
         while (j<3)
         {
             cont=0;
             for(int i=0; i<5; i++)
             {
-                if (Notas[j][i]>=6)
+                if (Notas[j][i]>6)
                 {
                     cont+=1;
                 }
             }
             Porcentaje[j]=(cont*100)/5;
+            printf("%s : %.2f\n", Trim[j], Porcentaje[j]);
             j++;
         }
 }
 
-void PorcenGeneral (float Suma1, float Promedios[4])
+void PromGeneral (float Promedios[4])
 {
+    float Promedio, Suma=0;
     for(int i=0; i<5; i++)
         {
-            Suma1=Suma1+Promedios[i];
+            Suma=Suma+Promedios[i];
         }
-        printf("Promedio general: %.1f\n\n", Suma1/5);
+        Promedio=Suma/5;
+        printf("El promedio general es: %.2f", Promedio);
 }
-
